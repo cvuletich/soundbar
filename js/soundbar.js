218fe4c78135c7106dc452ba667ca44cc86dff70
Chris Vuletich <cvuletich@gmail.com>
2012-11-13 19:57:04 -0600

    Rewritten from scratch

--- js/soundbar.js
+++ js/soundbar.js
@@ -1,4 +1,94 @@
-var sounds = {
+var audiopath = 'http://localhost/php/soundbar/audio/';
+
+(function(){
+	var v = "1.8.2";
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initbookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initookmarklet();
+	}
+
+	function initbookmarklet() {
+		(window.bookmarklet = function() {
+      soundbar.init();
+		})();
+	}
+})();
+
+var soundbar = {
+  close : function() {
+    $('#soundbar').remove();
+  },
+  init : function() {
+    var audiopath = '';
+    var bar = $('<div id="soundbar"></div>').css({
+      'background-color' : '#999',
+      'border-left' : '1px solid #666',
+      'font-family' : 'arial',
+      'height' : $(window).height(),
+      'right' : 0,
+      'position' : 'absolute',
+      'top' : 0,
+      'width' : '200px'
+    });
+    $('body').append(bar);
+    for (var key in soundbar.sounds) {
+      var html = $('<span>' + key + '</span>').css({
+        '-moz-box-shadow' : 'inset 0px 1px 0px 0px #ffffff',
+        '-webkit-box-shadow' : 'inset 0px 1px 0px 0px #ffffff',
+        'box-shadow' : 'inset 0px 1px 0px 0px #ffffff',
+        'cursor' : 'pointer',
+        'background-color' : '#ededed',
+        '-moz-border-radius' : '6px',
+        '-webkit-border-radius' : '6px',
+        'border-radius' : '6px',
+        'border' : '1px solid #dcdcdc',
+        'display' : 'inline-block',
+        'color' : '#777777',
+        'font-family' : 'arial',
+        'font-size' : '13px',
+        'font-weight' : 'bold',
+        'padding' : '6px 24px',
+        'text-decoration' : 'none',
+        'text-shadow' : '1px 1px 0px #ffffff',
+        'margin-top' : '4px',
+        'width' : '140px'
+      }).click(function() {
+        soundbar.play(key);
+      });
+      $(bar).append(html);
+    }
+    var close = $('<div>Close Soundbar</div>')
+      .css({
+        'bottom' : '5px',
+        'color' : '#666',
+        'cursor' : 'pointer',
+        'font-size' : '11px',
+        'left' : '5px',
+        'position' : 'absolute'
+      })
+      .click(function() { soundbar.close(); });
+    $(bar).append(close);
+  },
+  play : function(key) {
+    $('audio').remove();
+    var audio = $('<audio id="audio" src="' + audiopath + soundbar.sounds[key] + '"></audio>');
+    $('#soundbar').append(audio);
+    document.getElementById('audio').play();
+  },
+  resize : function() {
+    $('#soundbar').css('height', $(window).height());
+  },
+  sounds : {
    'Let the hate...' : 'hate.wav',
    'HIYOOOO!' : 'hiyoooo.wav',
    'It\'s a trap!' : 'itsatrap.wav',
@@ -7,89 +97,7 @@ var sounds = {
    'More than this' : 'morethanthis.wav',
    'Talk to the hand' : 'talktothehand.wav',
    'Yeah Baby!' : 'yeahbaby.wav'
-};
-
-var soundboardactive = false;
-
-function init(){
-    var closewindow = document.createElement('a');
-    var soundbar = document.createElement('div');
-    var height = (typeof window.innerHeight != 'undefined' ? window.innerHeight : document.body.offsetHeight);
-    soundbar.id = 'soundboard';
-    soundbar.style.backgroundColor = '#DDDDDD';
-    soundbar.style.borderLeft = '1px solid #AAAAAA';
-    soundbar.style.height = height + 'px';
-    soundbar.style.position = 'fixed';
-    soundbar.style.right = '0px';
-    soundbar.style.top = '0px';
-    soundbar.style.width = '180px';
-    soundbar.style.textAlign = 'center';
-    soundbar.style.zIndex = '2147483647';
-    var header = document.createElement('div');
-    header.id = 'soundbarheader';
-    header.innerHTML = 'SoundBar';
-    header.style.fontFamily = 'Arial';
-    header.style.fontWeight = 'bold';
-    header.style.fontSize = '18px';
-    header.style.paddingTop = '5px';
-    soundbar.appendChild(header);
-    for(var key in sounds){
-        soundbutton = document.createElement('input');
-        linebreak = document.createElement('br');
-        soundbutton.id = key;
-        soundbutton.style.marginTop = '4px';
-        soundbutton.style.width = '150px';
-        soundbutton.type = 'button';
-        soundbutton.value = key;
-        soundbutton.onclick = function(){ disableButton(this); playSound(sounds[this.id]); }
-        soundbar.appendChild(soundbutton);
-        soundbar.appendChild(linebreak);
-    }
-    closewindow.style.bottom = '2px';
-    closewindow.style.color = '#666666';
-    closewindow.style.fontFamily = 'helvetica';
-    closewindow.style.fontSize = '10px';
-    closewindow.style.left = '2px';
-    closewindow.style.position = 'absolute';
-    closewindow.href = 'javascript:closeSoundboard()';
-    closewindow.innerHTML = 'Close Window';
-    soundbar.appendChild(closewindow);
-    document.body.appendChild(soundbar);
-    soundboardActive = true;
+  }
 }
 
-function closeSoundboard(){
-    var soundboard = document.getElementById('soundboard');
-    document.body.removeChild(soundboard);
-}
-
-function disableButton(button){
-    button.disabled = true;
-    setTimeout(function(){ button.disabled = false; }, 5000);
-}
-
-function playSound(sound){
-    var soundbar = document.getElementById('soundboard');
-    if(document.getElementById('audio')){
-        var audio = document.getElementById('audio');
-        audio.parentNode.removeChild(audio);
-    }
-    var audio = document.createElement('audio');
-    audio.setAttribute('id', 'audio');
-    audio.src = 'http://www.chrisvuletich.com/soundbar/audio/' + sound;
-    soundbar.appendChild(audio);
-    audio.play();
-}
-
-function resizeSoundboard(){
-    if(soundboardactive){
-        var height = (typeof window.innerHeight != 'undefined' ? window.innerHeight : document.body.offsetHeight);
-        var soundboard = document.getElementById('soundboard');
-        soundbar.style.height = height + 'px';
-    }
-}
-
-if(!document.getElementById('soundboard') || document.getElementById('soundboard').style.visibility == 'hidden'){
-    init();
-}
-window.onresize = resizeSoundboard;
+window.onresize = soundbar.resize;
